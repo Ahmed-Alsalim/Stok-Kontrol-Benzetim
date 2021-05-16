f5c3a293dfb822a4197a44cee09ec026a5a4ddba
Ahmed Tawfiq <Tawfiq.Ahmed@ogr.sakarya.edu.tr>
2021-05-16 19:23:43 +0300

    Genel iyileştirmeler

--- Benzetim2.py
+++ Benzetim2.py
@@ -2,39 +2,26 @@
 """   B181200553     """
 """     Ödev 2       """
 
+from math import inf
 import random
 from beautifultable import BeautifulTable
 
-############
-Demo = True
-############
 
+######### Kullanılacak veriler #########
+TalepMin = 80
+TalepMax = 130
+BasStok = 224
+TedarikSure = [2, 3]
+TedarikSureOlasilik = [0.75, 0.25]
+SiparisMiktar = [224, 275, 325, 400]
+MinStok = [200, 225, 250, 275, 300]
+KayipMaliye = 100
+StokMaliye = 0.2
+SiparisMaliye = 50
+DevreSayi = 5
+BenzetimTablolarYazdir = False
+########################################
 
-if not Demo:
-    TalepMin, TalepMax = input(
-        "Talep dağılımın aralığı (arada boşluk bırakarak 'Min Max' şeklinde yazınız): "
-    ).split()
-    # Olasılık dağılımı ile yapılabilir sonra
-    # TedarikSure = input("Tedarik süresi: ")
-    Stok = input("Başlangıç stok: ")
-    # Siparis = input("Sipariş miktarı (Q): ")
-    # MinStok = input("Yeniden sipariş noktası (R): ")
-    KayipMaliye = input("Kayıp satış maliyeti / Adet: ")
-    StokMaliye = input("Elde bulundurma maliyeti / Adet: ")
-    SiparisMaliye = input("Sipariş maliyeti: ")
-    DevreSayi = input("Çalıştırılacak devre sayısı: ")
-
-else:
-    # Demo data
-    TalepMin = 80
-    TalepMax = 130
-    BasStok = 224
-    Siparis = [224, 275, 325, 400]
-    MinStok = [200, 225, 250, 275, 300]
-    KayipMaliye = 100
-    StokMaliye = 0.2
-    SiparisMaliye = 50
-    DevreSayi = 5
 
 BenZam = list(range(1, DevreSayi + 1))
 NihaiMaliyeList = []
@@ -52,7 +39,7 @@ ToplamMaliyeList = []
 
 
 def main():
-    for Q in Siparis:
+    for Q in SiparisMiktar:
         GeciciMaliyeList = []
         for R in MinStok:
             ClearData()
@@ -99,8 +86,9 @@ def main():
 
             Toplam = round(sum(ToplamMaliyeList), 1)
             GeciciMaliyeList.append(Toplam)
-            BenzetimTablo()
-            print("Q={} ve R={} için, Toplam maliyet: {} \n\n".format(Q, R, Toplam))
+            if BenzetimTablolarYazdir:
+                BenzetimTablo()
+                print("Q={} ve R={} için, Toplam maliyet: {} \n\n".format(Q, R, Toplam))
 
         NihaiMaliyeList.append(GeciciMaliyeList)
 
@@ -124,7 +112,7 @@ def TalepUret():
 
 
 def TedarikSureUret():
-    return int(random.choices([2, 3], weights=[0.75, 0.25])[0])
+    return int(random.choices(TedarikSure, weights=TedarikSureOlasilik)[0])
 
 
 def BenzetimTablo():
@@ -148,12 +136,26 @@ def BenzetimTablo():
 
 
 def KararTablo():
+    minindex = []
+    minvalue = inf
+    for i in NihaiMaliyeList:
+        if min(i) < minvalue:
+            minvalue = min(i)
+            minindex = [NihaiMaliyeList.index(i), i.index(min(i))]
+    NihaiMaliyeList[minindex[0]][minindex[1]] = "> {} <".format(minvalue)
+
     Table = BeautifulTable()
     Table.columns.append(MinStok, "R \\ Q")
     for col in range(len(NihaiMaliyeList)):
-        Table.columns.append(NihaiMaliyeList[col], str(Siparis[col]))
+        Table.columns.append(NihaiMaliyeList[col], str(SiparisMiktar[col]))
 
+    Table.set_style(BeautifulTable.STYLE_GRID)
     print(Table)
+    print(
+        "En düşük maliyet ({}) için, Sipariş miktarı (Q) = {} ve Yeniden sipariş noktası (R) = {} ".format(
+            minvalue, SiparisMiktar[minindex[0]], MinStok[minindex[1]]
+        )
+    )
 
 
 if __name__ == "__main__":
